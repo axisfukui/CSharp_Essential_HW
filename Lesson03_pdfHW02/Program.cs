ed4c2f5d1d3d638af5bd7c269552b01ddef10f33
NikolaiC <game.mmusor@gmail.com>
2020-04-13 10:39:53 +0200

    Lesson03_pdfHW02

---
+++ Lesson03_pdfHW02/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_pdfHW02
+{
+    //Создать класс, представляющий учебный класс ClassRoom.
+    //Создайте класс ученик Pupil.В теле класса создайте методы void Study(), void Read(), void
+    //Write(), void Relax().
+    //Создайте 3 производных класса ExcelentPupil, GoodPupil, BadPupil от класса базового класса
+    //Pupil и переопределите каждый из методов, в зависимости от успеваемости ученика.
+    //Конструктор класса ClassRoom принимает аргументы типа Pupil, класс должен состоять из 4 учеников.
+    //Предусмотрите возможность того, что пользователь может передать 2 или 3 аргумента.
+    //Выведите информацию о том, как все ученики экземпляра класса ClassRoom умеют учиться, читать,
+    //писать, отдыхать.
+    class ClassRoom
+    {
+        Pupil[] pupils;
+        public ClassRoom(Pupil pupil1, Pupil pupil2)
+            : this(pupil1, pupil2, null, null)
+        {
+
+        }
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3)
+           : this(pupil1, pupil2, pupil3, null)
+        {
+
+        }
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3, Pupil pupil4)
+        {
+            pupils = new Pupil[] { pupil1, pupil2, pupil3, pupil4 };
+        }
+        public void GetPupilInformation()
+        {
+            for (int i = 0; i < pupils.Length; i++)
+            {
+                pupils[i]?.Study();
+                pupils[i]?.Write();
+                pupils[i]?.Read();
+                pupils[i]?.Relax();
+                Console.WriteLine(new string('-',50));
+            }
+        }
+    }
+    class Pupil
+    {
+        public virtual void Study() { }
+        public virtual void Read() { }
+        public virtual void Write() { }
+        public virtual void Relax() { }
+    }
+    class ExcelentPupil : Pupil
+    {
+        public override void Study() { Console.WriteLine("ExcelentPupil Study"); }
+        public override void Read() { Console.WriteLine("ExcelentPupil Read"); }
+        public override void Write() { Console.WriteLine("ExcelentPupil Write"); }
+        public override void Relax() { Console.WriteLine("ExcelentPupil Relax"); }
+    }
+    class GoodPupil : Pupil
+    {
+        public override void Study() { Console.WriteLine("GoodPupil Study"); }
+        public override void Read() { Console.WriteLine("GoodPupil Read"); }
+        public override void Write() { Console.WriteLine("GoodPupil Write"); }
+        public override void Relax() { Console.WriteLine("GoodPupil Relax"); }
+    }
+    class BadPupil : Pupil
+    {
+        public override void Study() { Console.WriteLine("BadPupil Study"); }
+        public override void Read() { Console.WriteLine("BadPupil Read"); }
+        public override void Write() { Console.WriteLine("BadPupil Write"); }
+        public override void Relax() { Console.WriteLine("BadPupil Relax"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassRoom classRoom = new ClassRoom(new ExcelentPupil(), new GoodPupil(), new BadPupil(), new GoodPupil());
+
+            classRoom.GetPupilInformation();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson03_pdfHW_extra/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_pdfHW_extra
+{
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Требуется:
+    //Создайте класс Printer.
+    //В теле класса создайте метод void Print(string value), который выводит на экран значение
+    //аргумента.
+    //Реализуйте возможность того, чтобы в случае наследования от данного класса других классов, и вызове
+    //соответствующего метода их экземпляра, строки, переданные в качестве аргументов методов,
+    //выводились разными цветами.
+    //Обязательно используйте приведение типов.
+
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class RedPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            base.Print(value);
+
+            Console.ForegroundColor = c;
+
+        }
+        public void PrintRed()
+        {
+            Console.WriteLine("Red");
+        }
+    }
+    class GreenPrinter : Printer
+    {
+        public void Print(string value)
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            base.Print(value);
+
+            Console.ForegroundColor = c;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            printer.Print("Print text");
+
+            Printer redPrinter = new RedPrinter(); //Приведение к типу?
+            redPrinter.Print("redPrinter text"); 
+
+            Printer redPrinterUP = redPrinter as Printer;     /* Эквивалентно строке 63 ?*/
+            redPrinterUP.Print("redPrinter text after upCast");
+
+            RedPrinter redprinterDOWN = redPrinter as RedPrinter;
+            redprinterDOWN.Print("redPrinterUP after downCast");
+
+
+            Printer gp = new GreenPrinter();
+            gp.Print("Green Printer text");
+
+            Printer gpUP = gp as Printer; //UP Cast
+            gpUP.Print("Green Printer text after UP Cast");
+
+            GreenPrinter gpDown = gpUP as GreenPrinter; //DownCast
+            gpDown.Print("Green Printer text after DOWN Cast");
+            Console.ReadLine();
+        }
+    }
+}
