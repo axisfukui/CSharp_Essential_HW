ca4ba75ca007beb3f5eda67e180065adef5e5079
NikolaiC <game.mmusor@gmail.com>
2020-04-13 20:44:08 +0200

    Lesson03_pdfHW04

---
+++ Lesson03_pdfHW04/Workers/ExpertDocumentWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_pdfHW04.Workers
+{
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
